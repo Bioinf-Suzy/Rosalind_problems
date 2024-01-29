e712adcaed73729dba1c29a9f41877db54ef0fb0
Bioinf-Suzy <149475170+Bioinf-Suzy@users.noreply.github.com>
2024-01-29 09:21:04 +0100

    Update iprb.py
    correct code added + reading from input file

--- iprb.py
+++ iprb.py
@@ -1,15 +1,21 @@
 #iprb
-def iter_factorial(n):
-    result = 1
-    for i in range(2, n+1):
-        result *= i
-    return result
+with open('rosalind_iprb.txt') as f:
+    k,m,n = [int(x) for x in next(f).split()]
 
-def rec_factorial(n):
-    if n == 1:
-        return 1
-    else:
-        res = n * rec_factorial(n-1)
-        return res
-      
-print(rec_factroial(4))
+def dominant_prob(k,m,n):
+    t = k+m+n
+
+    kk = k/t * ((k-1)/(t-1))
+    km = k/t * (m/(t-1))
+    kn = k/t * (n/(t-1))
+
+    mm = (m/t)*((m-1)/(t-1))
+    mk = (m/t)*(k/(t-1))
+    mn = (m/t)*(n/(t-1))
+
+    nk = (n/t)*(k/(t-1))
+    nm = (n/t)*(m/(t-1))
+
+    prob = round((kk + km + kn + mk + nk + 0.75*mm + 0.5*mn + 0.5*nm),5)
+    return prob
+print(dominant_prob(k,m,n))
